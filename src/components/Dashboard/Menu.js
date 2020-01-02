97a9485d92ce7c249a0f3c2f3d4b8c875e0bd575
JChartier001 <JChartier001@gmail.com>
2020-01-02 12:51:25 -0600

    notification bar working. calendar imported to dashboard.
    Co-authored-by: Jackson Ogles <jackson.ogles28@gmail.com>
    Co-authored-by: Michael <michael.curry.dev@gmail.com>

--- src/components/Dashboard/Menu.js
+++ src/components/Dashboard/Menu.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Menu = () => {
+  const [date, setDate] = useState({ date: '' })
+
+	const handleChange = e => {
+		setDate({
+			...date,
+			[e.target.name]: e.target.value
+		});
+	};
+
   return (
     <>
       <div className='menuUserBox'>
@@ -9,10 +18,16 @@ const Menu = () => {
       </div>
 
       <div>
-        <div> Dashboard </div>
-        <div> My Plants </div>
-        <div> Tasks </div>
-        <div> Notes </div>
+        <div> <img src='images/Menu/dashboard.svg'/> Dashboard </div>
+        <div> <img src='images/Menu/myplants.svg'/> My Plants </div>
+        <div> <img src='images/Menu/tasks.svg'/> Tasks </div>
+        <div> <img src='images/Menu/notes.svg'/> Notes </div>
+        <input
+          type='date'
+          name='date'
+          value={date.date}
+          onChange={handleChange}
+        />
       </div>
 
       <div>
--- src/components/Dashboard/Notification.js
+++ src/components/Dashboard/Notification.js
@@ -3,7 +3,12 @@ import authAxios from '../../util/authAxios';
 
 
 const Notification =() => {
-    const [notification, setNotification] = useState();
+    const [notification, setNotification] = useState({
+        plants: [],
+        tasks: [],
+        notifications: [],
+        notes: []
+    });
 
     useEffect(() => {
         authAxios()
@@ -19,43 +24,69 @@ const Notification =() => {
                 
     return(
         <div>
-            {notification && notification.length ? notification.map(notif => {
-                return(
             <div className="notifBar">
-                if(notification.plants >= 0){
-                    <div className='plants'>
-                        <img src='images\Notifications\leaf_green.svg' alt='green leaf'/>
-                        <p>{notification.plant.length}</p>
-                        <p>Plants</p>
-                    </div>
-                } else {
+                {notification.plants && notification.plants.length !== 0 ?                  
+                    notification.plants.map(notif => {
+                        return(
+                        <div className='plants'>
+                            <img src='images\Notifications\leaf_green.svg' alt='green leaf'/>
+                            <p>{notification.plants.length}</p>
+                            <p>Plants</p>
+                        </div>
+                        )}):                
                     <div className='plantsNo'>
                         <img src='images\Notifications\leaf_black.svg' alt='black leaf'/>
                         <p>0</p>
                         <p>Plants</p>
                     </div>
                 }
-                if(notification.task >= 0){
-                    <div className='task'>
-                        <img src='images\Notifications\task_green.svg' alt='green task'/>
-                        <p>{notification.task.length}</p>
+                {notification.tasks && notification.tasks.length !== 0 ?                 
+                    notification.tasks.map(notif => {
+                        return(
+                        <div className='tasks'>
+                            <img src='images\Notifications\task_green.svg' alt='green task'/>
+                            <p>{notif.tasks.length}</p>
+                            <p>Tasks</p>
+                        </div>
+                        )}):                
+                    <div className='tasksNo'>
+                        <img src='images\Notifications\task_black.svg' alt='black task'/>
+                        <p>0</p>
                         <p>Tasks</p>
                     </div>
-                } else {
-                    <div className='plantsNo'>
-                        <img src='images\Notifications\leaf_black.svg' alt='black leaf'/>
-                        <p>0</p>
-                        <p>Plants</p>
-                    </div>
                 }
-
-
+                {notification.notifications && notification.notifications.length !== 0 ?                 
+                    notification.notifications.map(notif => {
+                        return(
+                        <div className='notifications'>
+                            <img src='images\Notifications\notification_green.svg' alt='green notification'/>
+                            <p>{notif.notifications.length}</p>
+                            <p>Notifications</p>
+                        </div>
+                        )}):                
+                        <div className='notificaitonsNo'>
+                            <img src='images\Notifications\notification_black.svg' alt='black notification'/>
+                            <p>0</p>
+                            <p>Notifications</p>
+                        </div>
+                }
+                    {notification.notes && notification.notes.length !== 0 ?                 
+                    notification.notes.map(notif => {
+                        return(
+                        <div className='notifications'>
+                            <img src='images\Notifications\notes_green.svg' alt='green notes'/>
+                            <p>{notif.notes.length}</p>
+                            <p>Notes</p>
+                        </div>
+                        )}):                
+                        <div className='tasksNo'>
+                            <img src='images\Notifications\notes_black.svg' alt='black notes'/>
+                            <p>0</p>
+                            <p>Notes</p>
+                        </div>
+                }
             </div>
-            
-            )}):null}
         </div>
-            
-
     )
 }
 export default Notification;
