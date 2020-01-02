3c8787b26e11701df91248e24296b66037b966d5
JChartier001 <JChartier001@gmail.com>
2020-01-02 10:25:40 -0600

    added router to index.js, added Route to app.js, working on login and register
    Co-authored-by: Jackson Ogles <jackson.ogles28@gmail.com>

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,34 @@
 import React, {useState} from 'react'
 
 
-const Login = () -> {
-    const [user, setUser] = useState();
+const Login = () => {
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const handleSubmit =e => {
+        e.preventDefault();
+        axiosAuth()
+        .post('/api/login', user)
+        .then(response => {
+            localStorage.setItem('token', response.data.token)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    };
+
+        return 
+
+
 }
+
+export default Login;
