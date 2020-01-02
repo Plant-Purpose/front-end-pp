a22c1c43d987be3247ad1251e7b0649ad37c67d0
JChartier001 <JChartier001@gmail.com>
2020-01-02 10:27:53 -0600

    added protected route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Route} from "react-router-dom";
 function App() {
 	return (
 		<div className='App'>
-			<p>test</p>
+			<Route exact path=
 		</div>
 	);
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,72 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Register = () => {
+const Register = (props) => {
 
   return (
     <>
-      const [state, setState] = useState({
-    email: '',
-    password: '',
-  });
-  const onChange = (e) => {
-    setState({
-      ...state,
-      [e.target.name]:e.target.value,
-    })
-  }
-  
-  const onSubmit = (e) => {
-    e.preventDefault();
-    props.registerUser(state, props);
-  }
-  return (
-    <>
-    {/* <NavBarOther/>
-    <MainContainer>
- 
-      <ImgContainer>
-        <img src='https://images.unsplash.com/photo-1566453838084-7ec27e71b3ca?ixlib=rb-1.2.1ixid=eyJhcHBfaWQiOjEyMDd9auto=formatfit=cropw=932q=80' style={{width: '30rem'}}alt='electricion working on lines' />
-      </ImgContainer>
-      <FormContainer>
-        <Title>
-        <h1>Register</h1>
-        
-        </Title>
-        <form onSubmit={onSubmit}>
-        <FormEntry>
-            <input
-              type='email' 
-              name='email' 
-              placeholder='E-mail'
-              value={props.email}
-              onChange={onChange}
-            >
-            </input>
-          </FormEntry>
-          <FormEntry>
-            <input
-              type='password' 
-              name='password' 
-              placeholder='Password'
-              value={props.password}
-              onChange={onChange}
-            >
-            </input>
-          </FormEntry>
-          <button type='submit' >Register</button>
-          <div style={{display: 'flex', justifyContent: 'center', margin: 'auto'}}>
-            <p style={{marginTop: '8px'}}>Already a Member? </p>
-            <Link to="/login" style={{textDecoration: 'none', fontSize: '1.5rem'}}> <button>Login</button></Link>
-          </div>
-          
-        </form>
-      </FormContainer>
-      
-    </MainContainer> */}
-    </>
-  )
-};
-
+      const [state, set]
     </>
   )
 }
