3b0d2f5779abacc3a9f07bb4c729d7a12b96db9c
JChartier001 <JChartier001@gmail.com>
2020-01-02 19:13:59 -0600

    v

--- src/components/AddPlant.js
+++ src/components/AddPlant.js
@@ -37,7 +37,7 @@ const AddPlant = (props) => {
     const handleSearch = e => {
         e.preventDefault();
         axios
-        .get(`https://trefle.io/api/plants?token=elpiZ21wT1JXZFVzemlubmx0VlRJZz09`. {
+        .get(`https://trefle.io/api/plants?token=elpiZ21wT1JXZFVzemlubmx0VlRJZz09`, {
             headers: {
                 "Access-Control-Max-Age": 86400
             }
