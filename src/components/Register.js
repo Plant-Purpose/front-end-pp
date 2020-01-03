40d26056c82a7324a0086762738757f647e4fa46
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-01-03 06:48:55 -0500

    added bg pic to login and register components

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,109 +1,117 @@
 import React from 'react';
-import axios from "axios";
-import { validateRegistration } from '../util/formValidation/validateRegistration'
+import axios from 'axios';
+import { validateRegistration } from '../util/formValidation/validateRegistration';
 import { useForm } from '../util/useForm';
 
 const initialState = {
-  full_name: '',
-  email: '',
-  password: '',
-  phone: '',
-  location: ''
-}
+	full_name: '',
+	email: '',
+	password: '',
+	phone: '',
+	location: ''
+};
 
 const Register = props => {
-  const { values: user, handleChange, errors, handleSubmit } = useForm(
-    initialState,
-    validateRegistration,
-    submit
-  )
-  
-  function submit() {
-    axios
-    .post('https://plant-purpose.herokuapp.com/api/auth/register', user)
-    .then(res => {
-      console.log(res)
-      localStorage.setItem('token', res.data.token)
-      props.history.push('/dashboard')
-    })
-    .catch(err => {
-      console.dir(err)
-    })
-  }
+	const { values: user, handleChange, errors, handleSubmit } = useForm(
+		initialState,
+		validateRegistration,
+		submit
+	);
 
-  return (
-    <div className='login-register'>  
-      <div className='log-reg-head'>
-        <h2 
-          onClick={() => props.history.push('/login')}
-          style={{ cursor: 'pointer' }}
-        >
-          Sign In
-        </h2> 
-        <h2 className='active-header'>Sign Up</h2>
-      </div>
-      <div className='log-reg-desc'> 
-        <h2>Create an Account!</h2>
-        <p>Keep forgetting to water your plants?  No problem, we have the solution.  Sign up today.</p>
-      </div>
-      <form  onSubmit={handleSubmit}>
-      <label htmlFor="name">Name: </label>
-      <input
-        type='text' 
-        name='full_name' 
-        placeholder='Name'
-        value={user.name}
-        onChange={handleChange}
-      />
+	function submit() {
+		axios
+			.post('https://plant-purpose.herokuapp.com/api/auth/register', user)
+			.then(res => {
+				console.log(res);
+				localStorage.setItem('token', res.data.token);
+				props.history.push('/dashboard');
+			})
+			.catch(err => {
+				console.dir(err);
+			});
+	}
 
-      <p id='error-text'>{errors.full_name}</p>
+	return (
+		<div className='form-contain'>
+			<div className='login-register'>
+				<div className='log-reg-head'>
+					<h2
+						onClick={() => props.history.push('/login')}
+						style={{ cursor: 'pointer' }}
+					>
+						Sign In
+					</h2>
+					<h2 className='active-header'>Sign Up</h2>
+				</div>
+				<div className='log-reg-desc'>
+					<h2>Create an Account!</h2>
+					<p>
+						Keep forgetting to water your plants? No problem, we have the
+						solution. Sign up today.
+					</p>
+				</div>
+				<form onSubmit={handleSubmit}>
+					<label htmlFor='name'>Name: </label>
+					<input
+						type='text'
+						name='full_name'
+						placeholder='Name'
+						value={user.name}
+						onChange={handleChange}
+					/>
 
-      <label htmlFor="email">Email: </label>
-      <input
-        type='text' 
-        name='email' 
-        placeholder='E-mail'
-        value={user.email}
-        onChange={handleChange}
-      />
+					<p id='error-text'>{errors.full_name}</p>
 
-      <p id='error-text'>{errors.email}</p>
+					<label htmlFor='email'>Email: </label>
+					<input
+						type='text'
+						name='email'
+						placeholder='E-mail'
+						value={user.email}
+						onChange={handleChange}
+					/>
 
-      <label htmlFor="password">Password: </label>
-      <input
-        type='password' 
-        name='password' 
-        placeholder='Password'
-        value={user.password}
-        onChange={handleChange}
-      />
+					<p id='error-text'>{errors.email}</p>
 
-      <p id='error-text'>{errors.password}</p>
+					<label htmlFor='password'>Password: </label>
+					<input
+						type='password'
+						name='password'
+						placeholder='Password'
+						value={user.password}
+						onChange={handleChange}
+					/>
 
-      <label htmlFor="phone">Phone: </label>
-      <input
-        type='text' 
-        name='phone' 
-        placeholder='Phone'
-        value={user.phone}
-        onChange={handleChange}
-      />
+					<p id='error-text'>{errors.password}</p>
 
-      <p id='error-text'>{errors.phone}</p>
+					<label htmlFor='phone'>Phone: </label>
+					<input
+						type='text'
+						name='phone'
+						placeholder='Phone'
+						value={user.phone}
+						onChange={handleChange}
+					/>
 
-       <label htmlFor="location">Location: </label>
-      <input
-        type='text' 
-        name='location' 
-        placeholder='Location'
-        value={user.location}
-        onChange={handleChange}
-      />
+					<p id='error-text'>{errors.phone}</p>
 
-      <button className='button' type='submit'> Register </button>
-      </form>
-    </div>
-  )
-}
+					<label htmlFor='location'>Location: </label>
+					<input
+						type='text'
+						name='location'
+						placeholder='Location'
+						value={user.location}
+						onChange={handleChange}
+					/>
+
+					<button className='button' type='submit'>
+						{' '}
+						Register{' '}
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
 
 export default Register;
